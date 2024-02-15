bf2504e54a21b3415f5ce2f9fa042556cd7be8c4
Seongmock Yoo <sm.yoo@lge.com>
2024-02-15 09:48:22 +0900

    subtitle 변경

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -536,7 +536,8 @@ def updateDaumTV(metadata, media):
         subtitle = page.xpath('//p[@class="episode_desc"]/strong/text()')
         episode.summary = '\n'.join(txt.strip() for txt in page.xpath('//p[@class="episode_desc"]/text()')).strip()
         episode.originally_available_at = episode_date
-        episode.title = subtitle[0] if subtitle else str(episode_num)+'화'
+        epinum_subtitle = "제 %d화"%episode_num
+        episode.title = subtitle[0] if subtitle else epinum_subtitle
         episode.rating = None
         thumbs_url = originalImageUrlFromCdnUrl(page.xpath('//div[@class="wrap_player"]/div[@class="wrap_thumb"]/a/img/@src')[0])
         if thumbs_url not in episode.thumbs:
